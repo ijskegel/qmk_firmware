1e8b5daa4859708502634fddea276e4419f17491
IJskegel <ijskegel@gmail.com>
2023-03-19 12:51:23 +0100

    First working version of keyboard

--- keyboards/ijskegel/redox_rp2040/keymaps/default/keymap.c
+++ keyboards/ijskegel/redox_rp2040/keymaps/default/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,SYM_L           ,SYM_L   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,
      KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_ADPU         ,KC_ADEN ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_PGDN         ,KC_HOME ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
-     KC_LGUI ,KC_PPLS ,KC_PMNS ,KC_ALAS ,KC_CTPL ,KC_BSPC ,KC_DEL          ,KC_ENT  ,KC_SPC  ,KC_RALT ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
+     KC_PPLS ,KC_PMNS ,KC_LGUI ,KC_ALAS ,KC_CTPL ,KC_BSPC ,KC_DEL          ,KC_ENT  ,KC_SPC  ,KC_RALT ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
   ),
 
   [_SYMB] = LAYOUT(
