887b51c94371cde1e6f847b9008a9b93f3dbc8c4
IJskegel <ijskegel@gmail.com>
2023-03-21 20:59:38 +0100

    Add tapdance

--- keyboards/ijskegel/redox_rp2040/keymaps/default/keymap.c
+++ keyboards/ijskegel/redox_rp2040/keymaps/default/keymap.c
@@ -25,6 +25,20 @@ enum custom_keycodes {
 #define KC_ADEN LT(_ADJUST, KC_END)
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
 
+// tapdance settings
+enum {
+  TD_LBRC_LCBR = 0,
+  TD_RBRC_RCBR
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_LBRC_LCBR] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
+  [TD_RBRC_RCBR] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR)
+};
+
+#define TD_LBR TD(TD_LBRC_LCBR)
+#define TD_RBR TD(TD_RBRC_RCBR)
+
 /*
  * Row - column numbering for the two halves half of the keyboard, effectively creating a 5x7 matrix for each half
  *
@@ -51,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
      KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_MINS         ,KC_EQL  ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_BSPC ,
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC         ,KC_RBRC ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,TD_LBR          ,TD_RBR  ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
      KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_HOME         ,KC_PGDN ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADEN         ,KC_ADPU ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
      KC_HYPR ,KC_MEH  ,KC_LALT ,KC_LGUI ,KC_LCTL ,KC_SYBS ,KC_DEL          ,KC_ENT  ,KC_NASP ,KC_RCTL ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
